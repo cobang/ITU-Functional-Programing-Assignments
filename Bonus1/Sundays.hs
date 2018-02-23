fbb948eaa1388702f53626a2fc53fb7bf99a6def
Goksel Coban <goksel326@gmail.com>
2018-02-24 02:01:18 +0300

    Add an implemetation of sundays1 function without rest expression

--- Bonus1/Sundays.hs
+++ Bonus1/Sundays.hs
@@ -13,8 +13,24 @@ dayOfWeek y m d = mod (d + m' + k + k' + j' + 5 * j ) 7
             True      -> div (13 * (m + 13)) 5
             otherwise -> div (13 * (m + 1)) 5
 
+-- This function recursively checks first day of each month of each year in the given interval
+-- and counts sundays.
+-- Implemetation without rest expression.
 sundays1 :: Integer -> Integer -> Integer
 sundays1 start end = sundays' start 1
+    where
+        sundays' :: Integer -> Integer -> Integer
+        sundays' y m
+            | y > end   = 0
+            | m == 12   = if dayOfWeek y m 1 == 1 then sundays' nextY 1 + 1 else sundays' nextY 1
+            | otherwise = if dayOfWeek y m 1 == 1 then sundays' y nextM + 1 else sundays' y nextM
+            where
+                nextY = y + 1
+                nextM = m + 1
+
+-- Implemetation with rest expression.
+sundays1Rest :: Integer -> Integer -> Integer
+sundays1Rest start end = sundays' start 1
     where
         sundays' :: Integer -> Integer -> Integer
         sundays' y m
