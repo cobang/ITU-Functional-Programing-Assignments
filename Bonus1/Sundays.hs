f81c020e946ff2e0f21b361cf6375407bcc94725
Goksel Coban <goksel326@gmail.com>
2018-02-24 02:28:42 +0300

    Add tail recursive implementation of the sundays1 function

--- Bonus1/Sundays.hs
+++ Bonus1/Sundays.hs
@@ -11,8 +11,21 @@ dayOfWeek y m d = mod (d + m' + k + k' + j' + 5 * j ) 7
         m' :: Integer
         m' = if m <= 2 then div (13 * (m + 13)) 5 else div (13 * (m + 1)) 5
 
--- This function recursively checks first day of each month of each year in the given interval
+-- sundays1 functions recursively checks first day of each month of each year in the given interval
 -- and counts sundays.
+-- Tail recursive implementation.
+sundays1Tail :: Integer -> Integer -> Integer
+sundays1Tail start end = sundays' 0 start 1
+    where
+        sundays' :: Integer -> Integer -> Integer -> Integer
+        sundays' acc y m
+            | y > end   = acc
+            | m == 12   = if dayOfWeek y m 1 == 1 then sundays' (acc + 1) nextY 1 else sundays' acc nextY 1
+            | otherwise = if dayOfWeek y m 1 == 1 then sundays' (acc + 1) y nextM else sundays' acc y nextM
+            where
+                nextY = y + 1
+                nextM = m + 1
+
 -- Implemetation without rest expression.
 sundays1 :: Integer -> Integer -> Integer
 sundays1 start end = sundays' start 1
