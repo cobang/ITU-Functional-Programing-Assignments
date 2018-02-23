55b25195d77a249a3c56f2c9335dd8826858f144
Goksel Coban <goksel326@gmail.com>
2018-02-24 01:36:15 +0300

    Complate the sundays1 function

--- Bonus1/Sundays.hs
+++ Bonus1/Sundays.hs
@@ -10,17 +10,19 @@ dayOfWeek y m d = mod (d + m' + k + k' + j' + 5 * j ) 7
 
         m' :: Integer
         m' = case m <= 2 of
-            True -> div (13 * (m + 13)) 5
+            True      -> div (13 * (m + 13)) 5
             otherwise -> div (13 * (m + 1)) 5
 
 sundays1 :: Integer -> Integer -> Integer
-sundays1 start end = sundays' ? ?
+sundays1 start end = sundays' start 1
     where
         sundays' :: Integer -> Integer -> Integer
         sundays' y m
-            | y > end = ?
-            | otherwise = if dayOfWeek y m 1 == then rest + 1 else rest
+            | y > end = 0
+            | otherwise = if dayOfWeek y m 1 == 1 then rest + 1 else rest
             where
-            	nextY = ?
-            	nextM = ?
-            	rest = ?
+                nextY = y + 1
+                nextM = m + 1
+                rest = case m == 12 of
+                    True      -> sundays' nextY 1
+                    otherwise -> sundays' y nextM
