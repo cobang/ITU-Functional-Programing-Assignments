d0c0bf8ab34056f6fb8e58c9280d85fec938377b
Goksel Coban <goksel326@gmail.com>
2018-02-26 01:51:18 +0300

    Fix sunday calculations

--- Bonus1/sundays.py
+++ Bonus1/sundays.py
@@ -2,10 +2,14 @@ from math import floor
 
 
 def day_of_week(y, m, d):
-    j = y // 100
-    k = y % 100
+    # https://www.geeksforgeeks.org/zellers-congruence-find-day-date/
+    # According to acticle on Geek For Geeks, we should subtract 1, when we add 12 to month
     if m <= 2:
         m += 12
+        y -= 1
+
+    j = y // 100
+    k = y % 100
     t1 = floor(13 * (m + 1) / 5)
     t2 = floor(k / 4)
     t3 = floor(j / 4)
@@ -38,13 +42,21 @@ def days_in_month(m, y):
 
 def sundays2(start, end):
     n = 0
-    weekday = 2
+    # If we set 2 to weekday as default value, result will correct only when start year starts with tuesday.
+    # To fix this, I prefer to calculate this value.
+    weekday = day_of_week(start, 1, 1) - 1
+
     for y in range(start, end + 1):
         for m in range(1, 13):
-            days = days_in_month(m, y)
-            weekday += days % 7
+            # I changed order of if statement and other statements because in older version we are calculating
+            # the upcoming months start day.
+            # For example: sundays(2000, 2005) will also count Jan 2006 (Sunday).
             if weekday % 7 == 0:
                 n += 1
+
+            days = days_in_month(m, y)
+            weekday += days % 7
+
     return n
 
 
