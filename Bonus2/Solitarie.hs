0e8ab9b555c78923d408106e4892da2535c405d7
Goksel Coban <goksel326@gmail.com>
2018-04-07 12:32:10 +0300

    Add allSameColor function

--- Bonus2/Solitarie.hs
+++ Bonus2/Solitarie.hs
@@ -1,6 +1,6 @@
 module Solitarie where
 
-data Color = Red | Black deriving (Show)
+data Color = Red | Black deriving (Eq, Show)
 data Suit  = Clubs | Diamonds | Hearts | Spades deriving (Eq, Show)
 data Rank  = Num Int | Jack | Queen | King | Ace deriving (Eq, Show)
 data Card  = Card { suit :: Suit, rank :: Rank } deriving (Eq, Show)
@@ -21,3 +21,9 @@ cardValue c = case c of
 removeCard :: [Card] -> Card -> [Card]
 removeCard [] _       = error "c is not in the list"
 removeCard (c':cs') c = if c' == c then cs' else c' : removeCard cs' c
+
+allSameColor :: [Card] -> Bool
+allSameColor []               = error "there is no card in the list"
+allSameColor [c]              = True
+allSameColor [c1,c2]          = (cardColor c1) == (cardColor c2)
+allSameColor cs@(c1':c2':_) = if (cardColor c1') == (cardColor c2') then allSameColor (tail cs) else False
