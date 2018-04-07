b4258a68b9636197115c9e996df45b9d32e0f086
Goksel Coban <goksel326@gmail.com>
2018-04-07 12:15:09 +0300

    Add removeCard function

--- Bonus2/Solitarie.hs
+++ Bonus2/Solitarie.hs
@@ -1,9 +1,9 @@
 module Solitarie where
 
 data Color = Red | Black deriving (Show)
-data Suit  = Clubs | Diamonds | Hearts | Spades deriving (Show)
-data Rank  = Num Int | Jack | Queen | King | Ace deriving (Show)
-data Card  = Card { suit :: Suit, rank :: Rank } deriving (Show)
+data Suit  = Clubs | Diamonds | Hearts | Spades deriving (Eq, Show)
+data Rank  = Num Int | Jack | Queen | King | Ace deriving (Eq, Show)
+data Card  = Card { suit :: Suit, rank :: Rank } deriving (Eq, Show)
 data Move  = Draw | Discard Card deriving (Show)
 
 cardColor :: Card -> Color
@@ -17,3 +17,7 @@ cardValue c = case c of
     Card _ (Num n) -> n
     Card _ Ace     -> 11
     _              -> 10
+
+removeCard :: [Card] -> Card -> [Card]
+removeCard [] _       = error "c is not in the list"
+removeCard (c':cs') c = if c' == c then cs' else c' : removeCard cs' c
