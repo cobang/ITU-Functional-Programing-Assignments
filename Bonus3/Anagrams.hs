73ba9557dfc3cabe2fc50129b7dbefaf287bf931
Goksel Coban <goksel326@gmail.com>
2018-05-03 17:22:47 +0300

    Add wordCharCounts, sentenceCharCounts and dictCharCounts functions

--- Bonus3/Anagrams.hs
+++ Bonus3/Anagrams.hs
@@ -7,3 +7,14 @@ import Data.Char
 type Word = [Char]
 type Sentence = [Word]
 type CharCounts = [(Char, Int)]
+
+wordCharCounts :: Word -> CharCounts
+wordCharCounts w = zip cs $ map (\c -> length $ filter (== c) lw) cs where
+    lw = map toLower w
+    cs = nub lw
+
+sentenceCharCounts :: Sentence -> CharCounts
+sentenceCharCounts = wordCharCounts.concat
+
+dictCharCounts :: [Word] -> [(Word, CharCounts)]
+dictCharCounts d = zip d (map wordCharCounts d)
