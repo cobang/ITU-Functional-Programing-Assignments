bb4bf0018b9fbad456b5a05cab90d7e1e46d6674
Goksel Coban <goksel326@gmail.com>
2018-05-03 19:19:47 +0300

    Add dictWordsByCharCounts function

--- Bonus3/Anagrams.hs
+++ Bonus3/Anagrams.hs
@@ -9,10 +9,13 @@ type Sentence = [Word]
 type CharCounts = Map Char Int
 
 wordCharCounts :: Word -> Map Char Int
-wordCharCounts w = fromListWith (+) (map (\x -> (x, 1)) w)
+wordCharCounts w = fromListWith (+) $ map (\x -> (x, 1)) w
 
 sentenceCharCounts :: Sentence -> CharCounts
 sentenceCharCounts = wordCharCounts.concat
 
 dictCharCounts :: Sentence -> [(Word, CharCounts)]
-dictCharCounts d = zip d (map wordCharCounts d)
+dictCharCounts d = zip d $ map wordCharCounts d
+
+dictWordsByCharCounts :: [(Word, CharCounts)] -> Map CharCounts [Word]
+dictWordsByCharCounts x = fromListWith (++) $ map (\(t0, t1) -> (t1, [t0])) x
