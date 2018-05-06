b58c3ca691f1db5fb050d932bbaaa54a7323ca2f
Goksel Coban <goksel326@gmail.com>
2018-05-07 00:00:18 +0300

    Implement insert function

--- TermProject/Trie.hs
+++ TermProject/Trie.hs
@@ -1,6 +1,6 @@
 module Trie where
 import qualified Data.Map as M
-import Data.Maybe
+import Data.Maybe (fromMaybe)
 import System.Environment
 import System.IO
 import Prelude hiding (Word)
@@ -12,7 +12,8 @@ empty :: Trie
 empty = Trie {end=False, children=M.empty}
 
 insert :: Word -> Trie -> Trie
-insert = undefined
+insert [] t        = Trie {end=True, children=children t}
+insert (w1':ws') t = Trie {end=(end t), children=(M.insert w1' (insert ws' (fromMaybe empty (children t M.!? w1'))) (children t))}
 
 insertList :: [Word] -> Trie
 insertList = undefined
