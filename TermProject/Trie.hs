8bd912e0e132ea16fa7bb77deb78f741b9502afa
Goksel Coban <goksel326@gmail.com>
2018-05-06 22:09:54 +0300

    Implement empty function

--- TermProject/Trie.hs
+++ TermProject/Trie.hs
@@ -1,15 +1,15 @@
 module Trie where
-import Data.Map
+import qualified Data.Map as M
 import Data.Maybe
 import System.Environment
 import System.IO
 import Prelude hiding (Word)
 
-data Trie = Trie {end :: Bool, children :: Map Char Trie}
+data Trie = Trie {end :: Bool, children :: M.Map Char Trie} deriving (Show)
 type Word = String
 
 empty :: Trie
-empty = undefined
+empty = Trie {end=False, children=M.empty}
 
 insert :: Word -> Trie -> Trie
 insert = undefined
