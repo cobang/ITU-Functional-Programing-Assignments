2cbf8ef76dd296028b4bd1014b09e5fb1e38c3f4
Goksel Coban <goksel326@gmail.com>
2018-05-07 23:50:24 +0300

    Implement getWords function

--- TermProject/Trie.hs
+++ TermProject/Trie.hs
@@ -5,7 +5,7 @@ import System.Environment
 import System.IO
 import Prelude hiding (Word)
 
-data Trie = Trie {end :: Bool, children :: M.Map Char Trie} deriving (Show)
+data Trie = Trie {end :: Bool, children :: M.Map Char Trie} deriving (Eq, Show)
 type Word = String
 
 empty :: Trie
@@ -13,7 +13,7 @@ empty = Trie {end=False, children=M.empty}
 
 insert :: Word -> Trie -> Trie
 insert [] t        = Trie {end=True, children=children t}
-insert (w1':ws') t = Trie {end=(end t), children=(M.insert w1' (insert ws' (fromMaybe empty (children t M.!? w1'))) (children t))}
+insert (w1':ws') t = Trie {end=end t, children=M.insert w1' (insert ws' $ fromMaybe empty $ children t M.!? w1') (children t)}
 
 insertList :: [Word] -> Trie
 insertList = undefined
@@ -22,7 +22,18 @@ search :: Word -> Trie -> Bool
 search = undefined
 
 getWords :: Trie -> [Word]
-getWords = undefined
+getWords t = getNode "" [] t
+    where
+        getNode :: Word -> [Word] -> Trie -> [Word]
+        getNode acc l t' = foldr (\x y-> (getEdge acc [] x) ++ y) l (M.toList (children t'))
+            where
+                getEdge :: Word -> [Word] -> (Char, Trie) -> [Word]
+                getEdge acc'' accl'' (c'',t'') = case (end t'', (children t'')==M.empty) of
+                    (_, True)       -> (reverse w): accl''
+                    (True, False)   -> getNode w ((reverse w):accl'') t''
+                    (False, False)  -> getNode w accl'' t''
+                    where
+                        w = c'':acc''
 
 prefix :: Word -> Trie -> Maybe [Word]
 prefix = undefined
